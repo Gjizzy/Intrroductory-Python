886fb82db11f87f0af94e80b7fb1e31417f137e4
Gjizzy <Yaquelin.Lopez@sdgku.edu>
2025-08-17 12:55:07 -0700

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,17 @@
+#Functions
+#Is a block of code which only runs when it is called
+#We can pass data known as parameters and the function can return data
+
+#Simple functions without parameters
+def my_function():
+    print("This is my function")
+
+def other_funnction():
+    print("This is another function!")
+
+#Function with parameters
+def print_full_name(fname, flast_name):
+    print(f"The name is: {fname} {flast_name}")
+
+print_full_name("Yaquelin","Lopez")
+
---
+++ intro.py
@@ -0,0 +1,54 @@
+print("Hello world from Python!")
+print(2)
+print(5+3)
+print(True)
+print("Didnt save it")
+
+#This is a comment
+" " " 
+Multiple line comment
+" " " 
+#Creating variables
+name = "Yaquelin"
+age = 24
+middle_name = "Yasdi"
+last_name = "Lopez"
+found = "False"
+#Concatenation
+print(My name is: " + name + ", and I I'm " + str(age) + "years old.")
+print(Did he showed uo to the class? " + str(found))
+
+#The f format
+#f"" or f"""""""
+
+print (f"My name is: {name}, and I'm {age} years old!")
+print(f"""My name is: {name}
+I like programming in Python""")
+#The type () helps us to know the data type of the variable
+print(type(name)))
+print(type(age))
+print(type(True))
+
+#Casting
+#Helps us to convert differant data types
+#str() - convert from any data type to string 
+#int() - convert Sring numbers to numeric values
+#fLoat() - convert String number to float type
+print(20+int("20"))
+print(20+age)
+print(20+int(2.98))
+print(20+float("2.98"))
+
+#input method
+#is going to allow us to interact with the terminal and pass some values
+#input())
+# print(input())
+# print(input("Enter your name here: "))
+# print(type(input("Enter your age here: ")))
+new_age = int(input("Enter a new age: -"))
+# print(age + new_age)
+
+x = int(input("Enter a first value: "))
+y= int(input("Enter a second value: "))
+print(x+y)
+
